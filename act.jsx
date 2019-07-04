c5aa06628c5f24edcb372ccb290b9afa7590d6db
AnastasiyaPo <nace.poteryaeva@gmail.com>
2019-07-04 16:24:11 +0700

    слайдер
    Создан большой слайдер
    Нужно подумат еще над адаптацией размера картинок

--- act.jsx
+++ act.jsx
@@ -11,3 +11,42 @@
     goToTop.addEventListener('click', backToTop)
 })();
 
+
+var slideIndex = 1;
+
+function plusSlide(){
+    showSlides(slideIndex += 1, ' right');
+}
+function minusSlide(){
+    showSlides(slideIndex -= 1, ' left');
+}
+
+function showSlides(n, direction){
+    var i;
+    var slides = document.getElementsByClassName("item");
+    var dots = document.getElementsByClassName("slider_dots_item");
+    if (n > slides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].style.background = "rgb(182, 182, 182)";
+    }
+    
+    var elm = document.getElementsByClassName("slaider_img");
+    for (i = 0; i < elm.length; i++) {
+        elm[i].className = elm[i].className.replace(" left", "");
+        elm[i].className = elm[i].className.replace(" right", "");
+    }
+    elm[slideIndex - 1].className += direction;
+
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].style.background = "none";
+}
