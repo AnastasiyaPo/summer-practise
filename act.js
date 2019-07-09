c5beeebb633f0ea62206a59220b26d084bafa3cb
AnastasiyaPo <nace.poteryaeva@gmail.com>
2019-07-09 15:08:54 +0700

    Переделан маленький слайдер
    Маленький слайдер улучшен

--- act.js
+++ act.js
@@ -76,11 +76,11 @@ var small_slider = [['small_slider/picture_to_ss1.png','1Indonectetus facilis','
 var ss_len = small_slider.length;
 
 
-//добавление узла
+// //добавление узла
 
 for (i = 4; i < ss_len; i++){
  var div = document.createElement('div');
- div.className = "block_in_small_slider ss_passive";
+ div.className = "block_in_small_slider";
  div.id = "idss" + (i + 1);
  slider.appendChild(div);
  var idss = document.getElementById("idss" + (i + 1));
@@ -122,85 +122,26 @@ console.log("ширина браузера = " + document.body.clientWidth);
 // 	var ind_len = 1;
 
 
-var ss_index = Array(ind_len);
-for (i = 0; i < ind_len; i++)
-		ss_index[i] = i;
-
-
-
-function leaves(n){
-	var vr = ss_index[n];
-	pic[vr].className = pic[vr].className.replace(" ss_active_left", "");
-	pic[vr].className = pic[vr].className.replace(" ss_active_right", "");
-		pic[vr].className = pic[vr].className += " ss_passive";
-}
-function come_r(){
-	pic[ss_index[0] - 1].className = pic[ss_index[0] - 1].className.replace(" ss_passive", "");
-}
-function cancel_activity(direction){
-    if (direction.localeCompare("left") == 0){
-	    for (i = 0; i < ind_len; i++)
-            ss_index[i]++;
-    }
-    if (direction.localeCompare("right") == 0) {
-        for (i = 0; i < ind_len; i++)
-            ss_index[i]--;
-    }
-        
-	for (i = 0; i < pic_len; i++){
+function time_stop_to_left(){
+    slider.insertBefore(pic[0], null);
+    for (i = 0; i < pic_len; i++)
         pic[i].className = pic[i].className.replace(" ss_active_left", "");
-        pic[i].className = pic[i].className.replace(" ss_active_right", "");
-    }
-    
 }
-function cancel_the_end(direction){
-	if (direction.localeCompare("left") == 0){	
-		for (i = 0; i < pic_len; i++){
-			pic[i].className = pic[i].className.replace(" ss_active_left_end", "");
-		}		
-	}
-	if (direction.localeCompare("right") == 0){	
-		for (i = 0; i < pic_len; i++){
-			pic[i].className = pic[i].className.replace(" ss_active_right_end", "");
-		}		
-	}
+function time_stop_to_right(){
+    slider.insertBefore(pic[pic_len - 1], slider.firstChild);
+    for (i = 0; i < pic_len; i++)
+        pic[i].className = pic[i].className.replace(" ss_active_right", "");
 }
 function left_ss(){
-    console.log(ss_index);
-	if (ss_index[0] >= 0 && ss_index[ind_len - 1] > ss_index[0] && ss_index[0] < ss_len - ind_len){  
-		pic[ss_index[ind_len - 1] + 1].className = pic[ss_index[ind_len - 1] + 1].className.replace(" ss_passive", "");//для последнего элемента
-		
-		for (i = 0; i < (ind_len + 1); i++){
-			pic[ss_index[0] + i].className = pic[ss_index[0] + i].className += " ss_active_left"; //для середины
-		}
-	
-    setTimeout(leaves, 400, 0);
-    setTimeout(cancel_activity, 400, "left");	
-	}
-	else{
-		slider.insertBefore(pic[0], null);
-		for (i = 0; i < ind_len; i++)
-			ss_index[i]--;
-		left_ss();
-	}
-	
+    setTimeout(time_stop_to_left, 400);
+    for (i = 0; i < pic_len; i++){
+        	pic[i].className = pic[i].className += " ss_active_left"; 
+        }
 }
 
 function right_ss(){
-	if (ss_index[0] <= (pic_len - ind_len) && ss_index[0] > 0 && ss_index[ind_len - 1] > ss_index[0]){  
-	
-		for (i = 0; i < ind_len; i++){
-			pic[ss_index[0] + i].className = pic[ss_index[0] + i].className += " ss_active_right"; 
-		}
-	setTimeout(come_r, 400);
-	setTimeout(leaves, 400, ind_len - 1);
-    setTimeout(cancel_activity, 400, "right");			
-	}
-	else{
-		slider.insertBefore(pic[pic_len - 1], slider.firstChild);
-		for (i = 0; i < ind_len; i++)
-			ss_index[i]++;
-		right_ss();
-	}
-        
+    setTimeout(time_stop_to_right, 400);
+    for (i = 0; i < pic_len; i++){
+        	pic[i].className = pic[i].className += " ss_active_right"; 
+        }
 }
