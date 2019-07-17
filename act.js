bf34dbf2bc786d471ab5adb7f9ed6ec4d0b4490b
AnastasiyaPo <nace.poteryaeva@gmail.com>
2019-07-17 20:33:19 +0700

    Скроллинг 0,6секунд

--- act.js
+++ act.js
@@ -6,7 +6,7 @@
     function backToTop(){
         if (window.pageYOffset > 0){
             window.scrollBy(0, -10);
-            setTimeout(backToTop, 10);
+            setTimeout(backToTop, 3.5);
         }
     }
     var goToTop = document.getElementById("scrolltop");
