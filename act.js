677a5cd18e0182a0670db845e08340943b57d56e
AnastasiyaPo <nace.poteryaeva@gmail.com>
2019-07-20 21:40:32 +0700

    Хранение данных для маленького слайдера
    Перенесла хранение данных для маленького слайдера в HTML-файл

--- act.js
+++ act.js
@@ -69,52 +69,8 @@ function showSlides(n, direction){
 
 // маленький слайдер(не ограниченный без прокрутки)
 
-
-var small_slider = [['picture/picture_to_ss1.png','1Indonectetus facilis','1Fat new smallness few supposing suspicion two. Way own uncommonly trave'],
-                    ['picture/picture_to_ss2.png','2Indonectetus facilis','2Fat new smallness few supposing suspicion two. Way own uncommonly trave'],
-                    ['picture/picture_to_ss3.png','3Indonectetus facilis','3Fat new smallness few supposing suspicion two. Way own uncommonly trave'],
-                    ['picture/picture_to_ss4.png','4Indonectetus facilis','4Fat new smallness few supposing suspicion two. Way own uncommonly trave'],
-                    ['picture/picture_to_ss5.png','5Indonectetus facilis','5Fat new smallness few supposing suspicion two. Way own uncommonly trave'],
-                    ['picture/picture_to_ss6.png','6Indonectetus facilis','6Fat new smallness few supposing suspicion two. Way own uncommonly trave'],
-                    ['picture/picture_to_ss7.png','7Indonectetus facilis','7Fat new smallness few supposing suspicion two. Way own uncommonly trave'],
-            ];
-var ss_len = small_slider.length;
-
-
-// //добавление узла
-
-for (i = 4; i < ss_len; i++){
- var div = document.createElement('div');
- div.className = "block_in_small_slider";
- div.id = "idss" + (i + 1);
- slider.appendChild(div);
- var idss = document.getElementById("idss" + (i + 1));
- var img = document.createElement('img');
- img.id = "ss" + (i + 1) + "_img";
- img.align = "left";
- var text = document.createElement('div');
- text.className = "text";
- text.id = "text" + (i + 1);
- text.innerHTML = "<p class = name id = ss" + (i + 1) + "_name></p><p class = information_text id = ss" + (i + 1) + "_text>";
-
-
- idss.appendChild(img);
- idss.appendChild(text);
- img.src = small_slider[i][0];
- var elm = document.getElementById("ss" + (i + 1) + "_name");
- elm.innerHTML = small_slider[i][1];
- elm = document.getElementById("ss" + (i + 1) + "_text");
- elm.innerHTML = small_slider[i][2];
-}
-
 var pic = document.getElementsByClassName("block_in_small_slider");
 pic_len = pic.length;
-// начальное положение
-for (i = 0; i < pic_len; i++){
-    document.getElementById("ss" + (i + 1) + "_img").src = small_slider[i][0];
-    document.getElementById("ss" + (i + 1) + "_name").innerHTML = small_slider[i][1];
-    document.getElementById("ss" + (i + 1) + "_text").innerHTML = small_slider[i][2];
-    }
 
 function time_stop_to_left(){
     slider.insertBefore(pic[0], null);
